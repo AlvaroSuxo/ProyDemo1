d650f35173c2eb825b148989234f138134133969
Alvaro Suxo Marca <alvaro.diabol@gmail.com>
2022-11-08 10:26:08 -0400

    Carga Inicial

---
+++ ProyDemo1/ProyDemo1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyDemo1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
